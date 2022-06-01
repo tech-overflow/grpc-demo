e825c2a6c1b36e03573f159208488a41258ff7f9
snatarajan <nbsiva90@gmail.com>
2022-06-01 22:09:48 +0530

    gRPC client + server and basic stubbing

--- grpc-client/src/main/java/ClientDriver.java
+++ grpc-client/src/main/java/ClientDriver.java
@@ -1,5 +1,51 @@
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.techoverflow.publicapis.PublicApisGrpc;
+import io.techoverflow.publicapis.PublicApisRequest;
+import io.techoverflow.publicapis.PublicApisResponse;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ClientDriver {
-    public static void main(String[] args) {
-        System.out.println("Client");
+    private static final Logger logger = Logger.getLogger(ClientDriver.class.getName());
+
+    PublicApisGrpc.PublicApisBlockingStub blockingStub;
+
+    public ClientDriver(Channel channel) {
+        blockingStub = PublicApisGrpc.newBlockingStub(channel);
+    }
+
+    public void getApiList() {
+        PublicApisRequest request = PublicApisRequest.newBuilder().build();
+        try {
+            PublicApisResponse response = blockingStub.getApiList(request);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        String target = "localhost:50051";
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(target)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext()
+                .build();
+
+        try {
+            ClientDriver client = new ClientDriver(channel);
+            client.getApiList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // ManagedChannels use resources like threads and TCP connections. To prevent leaking these
+            // resources the channel should be shut down when it will no longer be used. If it may be used
+            // again leave it running.
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
     }
 }
--- grpc-server/src/main/java/ServerDriver.java
+++ grpc-server/src/main/java/ServerDriver.java
@@ -1,5 +1,69 @@
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import io.techoverflow.publicapis.PublicApisGrpc;
+import io.techoverflow.publicapis.PublicApisRequest;
+import io.techoverflow.publicapis.PublicApisResponse;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
 public class ServerDriver {
-    public static void main(String[] args) {
-        System.out.println("Server");
+    private static final Logger logger = Logger.getLogger(ServerDriver.class.getName());
+    private Server server;
+
+    private void start() throws IOException {
+        /* The port on which the server should run */
+        int port = 50051;
+        server = ServerBuilder.forPort(port)
+                .addService(new PublicApisGrpcImpl())
+                .build()
+                .start();
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                try {
+                    ServerDriver.this.stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace(System.err);
+                }
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    private void stop() throws InterruptedException {
+        if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+        }
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    static class PublicApisGrpcImpl extends PublicApisGrpc.PublicApisImplBase {
+
+        @Override
+        public void getApiList(PublicApisRequest req, StreamObserver<PublicApisResponse> responseObserver) {
+            PublicApisResponse reply = PublicApisResponse.newBuilder().build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ServerDriver server = new ServerDriver();
+        server.start();
+        server.blockUntilShutdown();
     }
 }
